2c4ab0916af4f7972f0ce1305da2e21e67ff07ed
Godwin Naces <godwinnaces@gmail.com>
2024-09-10 20:39:55 +0000

    polishing

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,9 +5,25 @@ const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal")
 document.getElementById("status").addEventListener("click", e => getStatus(e));
 document.getElementById("submit").addEventListener("click", e => postForm(e));
 
+function processOptions(form) {
+    let optArray = [];
+
+    for (let e of form.entries()) {
+        if (e[0] === "options") {
+            optArray.push(e[1]);
+        }
+    }
+
+    form.delete("options");
+
+    form.append("options", optArray.join());
+
+    return form;
+}
+
 async function postForm(e) {
 
-    const form = new FormData(document.getElementById("checksform"));
+    const form = processOptions(new FormData(document.getElementById("checksform")));
 
     const response = await fetch(API_URL, {
         method: "POST",
@@ -36,19 +52,18 @@ async function getStatus(e) {
     const data = await response.json();
 
     if (response.ok) {
-        displayErrors(data);
+        displayStatus(data);
     } else {
         throw new Error(data.error);
     }
 
 }
 
-
 function displayErrors(data) {
 
     let results = "";
-    let heading = `JSHint Results for ${data.file}`;
 
+    let heading = `JSHint Results for ${data.file}`;
     if (data.total_errors === 0) {
         results = `<div class="no_errors">No errors reported!</div>`;
     } else {
@@ -65,9 +80,6 @@ function displayErrors(data) {
     resultsModal.show();
 }
 
-
-
-
 function displayStatus(data) {
 
     let heading = "API Key Status";
@@ -77,4 +89,5 @@ function displayStatus(data) {
     document.getElementById("resultsModalTitle").innerText = heading;
     document.getElementById("results-content").innerHTML = results;
     resultsModal.show();
+
 }
